4c2ad4d630d2ea62ff4db38d369178d9666fd72d
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-28 04:41:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+    
 end
